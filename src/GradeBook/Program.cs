101c02a89d213a438ac9648935cb48a7dab0c40c
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2020-12-28 16:13:00 -0600

    Continuing with C# Fundamentals, 3rd module

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new[] {1.1, 2.2, 3.3, 4.4};
+
+            var sum = 0.0;
+            foreach(var number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine(sum);
+
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
